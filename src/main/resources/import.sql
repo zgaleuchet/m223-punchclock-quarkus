2c92f57adb8819295cf42233e929a162c50669c0
zgaleuchet <zoe.galeuchet@lernende.bbw.ch>
2021-11-17 10:48:10 +0100

    updated import statements for default values

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,14 +4,18 @@ INSERT INTO CATEGORY(titel) VALUES ('Implementierung');
 INSERT INTO CATEGORY(titel) VALUES ('Testen');
 INSERT INTO CATEGORY(titel) VALUES ('Finanzen');
 
-INSERT INTO PROJECT(titel) VALUES ('225');
-INSERT INTO PROJECT(titel) VALUES ('783');
-INSERT INTO PROJECT(titel) VALUES ('155');
-INSERT INTO PROJECT(titel) VALUES ('532');
-INSERT INTO PROJECT(titel) VALUES ('622');
+INSERT INTO PROJECT(titel, category_id) VALUES ('225', 1);
+INSERT INTO PROJECT(titel, category_id) VALUES ('783', 1);
+INSERT INTO PROJECT(titel, category_id) VALUES ('155', 2);
+INSERT INTO PROJECT(titel, category_id) VALUES ('532', 3);
+INSERT INTO PROJECT(titel, category_id) VALUES ('622', 4);
+
+INSERT INTO USERROLE(title) VALUES ('Admin');
+INSERT INTO USERROLE(title) VALUES ('User');
 
 
-INSERT INTO USER(firstname, lastname, username, password, USERROLE) VALUES ('Test', 'User', 'testUser', 'secret', 'User');
-INSERT INTO USER(firstname, lastname, username, password, USERROLE) VALUES ('Zoe', 'Galeuchet', 'zgaleuchet', 'passwort', 'Admin');
+INSERT INTO USER(firstname, lastname, username, password, userrole_id) VALUES ('Test', 'User', 'testUser', 'secret', 1);
+INSERT INTO USER(firstname, lastname, username, password, userrole_id) VALUES ('Zoe', 'Galeuchet', 'zgaleuchet', 'passwort', 2);
 
-INSERT INTO ENTRY(checkIn, checkOut, Category) VALUES ('')
+INSERT INTO ENTRY(checkIn, checkOut, user_id, category_id) VALUES ('2021-11-17T12:15:00', '2021-11-17T12:15:00', 1, 1);
+INSERT INTO ENTRY(checkIn, checkOut, user_id, category_id) VALUES ('2022-11-18T12:19:00', '2022-11-19T16:43:00', 1, 2);
