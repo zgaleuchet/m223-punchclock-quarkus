85165d8d11b72079cde20f30113e7371074b8865
zgaleuchet <zoe.galeuchet@lernende.bbw.ch>
2021-11-22 11:09:03 +0100

    added login request

--- src/main/java/ch/zli/m223/punchclock/controller/AuthentificationController.java
+++ src/main/java/ch/zli/m223/punchclock/controller/AuthentificationController.java
@@ -2,10 +2,12 @@ package ch.zli.m223.punchclock.controller;
 
 import ch.zli.m223.punchclock.domain.User;
 import ch.zli.m223.punchclock.service.AuthenticationService;
+import ch.zli.m223.punchclock.service.UserService;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path(("/auth"))
 public class AuthentificationController {
@@ -13,16 +15,23 @@ public class AuthentificationController {
     @Inject
     AuthenticationService authenticationService;
 
+    @Inject
+    UserService userService;
+
     @POST
-    @Path("/login")
-    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/login/{username}/{password}")
     @Produces(MediaType.TEXT_PLAIN)
-    public String login(User user){
-        if (authenticationService.checkIfUserExists(user)) {
-            return authenticationService.GenerateValidJwtToken(user.getUsername());
-        } else {
-            throw new NotAuthorizedException("User " + user.getUsername() + " was not found");
+    public String login(@PathParam("username") String username, @PathParam("password") String password) {
+        List<User> users = userService.getUserByUsername(username);
+        if (authenticationService.checkIfUserExists(users.get(0))) {
+            User user = users.get(0);
+            if (user.getPassword().equals(password)) {
+                return authenticationService.GenerateValidJwtToken(user.getUsername());
+            }
         }
+        return "";
+
+
     }
 
 
